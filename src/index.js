7fc0a312b7803defea204473f65d6bbc2fd05f2e
muazzam51214 <syedmuazzamabbas2004@gmail.com>
2024-06-07 23:35:37 +0500

    Utils (Custom API Error and Response)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config({path : "./.env"})
 import connectDB from "./db/index.js";
-connectDB();
+import { app } from "./app.js";
+const port = process.env.PORT;
+connectDB()
+.then(() => {
+  app.on("error", () => {
+    console.log("Error : ", error);
+    throw error;
+  })
+
+  app.listen(port, () => {
+    console.log(`App is listening http://localhost:${port}`);
+  })
+})
+.catch((error) => {
+  console.log("MongoDB Connection Failed!!", error);
+})
